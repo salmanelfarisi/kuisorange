b0a4e7df7a1e3747446927729cae0710616f6da0
salmanelfarisi <zyxfaris@gmail.com>
2012-11-28 08:13:41 +0700

    tes

---
+++ assets/js/init.js
@@ -0,0 +1,56 @@
+var init = {
+    'questions': 
+	[{
+        'question': '1. 9 x 9 - 72 / 8 + 1 = ...',
+        'answers': ['a. 2.25', 'b. 5', 'c. 73', 'd. 40','e. 25'],
+        'correctAnswer': 3
+    }, {
+        'question': '2. Tentukan bilangan asli terkecil yang selalu bersisa 2 jika dibagi 3, 4, 5, 6, atau 7!',
+        'answers': ['a. 1', 'b. 2', 'c. 100', 'd. 200','e. 422'],
+        'correctAnswer': 2    
+    }, {
+        'question': '3. Berapa banyak kemungkinan pasangan asli (a,b) sehingga KPK(a,b) = 63000 dan FPB(a,b) = 35?',
+        'answers': ['a. 32', 'b. 8', 'c. 40', 'd. 4','e. 16'],
+        'correctAnswer': 2
+	}, {
+		'question': '4. 5 + 5 x 5 - 5 + 5 x 5 = ...',
+        'answers': ['a. 0', 'b. 5 ', 'c. 25', 'd. 50','e. 125'],
+        'correctAnswer': 4
+    
+	}, {
+		'question': '5. 10 : 10 + 10 - 10 x 10 + 10 : 10= ...',
+        'answers': ['a. -100', 'b. -88', 'c. 2', 'd. 88','e. 100'],
+        'correctAnswer': 2
+    }, {
+		'question': '6. Waktu yang diperlukan planet untuk berputar satu kali disebut...',
+        'answers': ['a. orbit', 'b. ekliptika', 'c. kala revolusi', 'd. kala rotasi','e. kala ekliptika'],
+        'correctAnswer': 4
+	}, {
+		'question': '7. Benda dikatakan bergerak jika...',
+        'answers': ['a. mengalami perpindahan', 'b. mengalami proses', 'c. mengalami perubahan bentuk', 'd. mengalami perubahan suhu','e. mengalami perubahan susunan benda ini'],
+        'correctAnswer': 1
+	}, {
+		'question': '8. Pertambahan kecepatan dalam selang waktu tertentu disebut...',
+        'answers': ['a. berkecepatan', 'b. percepatan', 'c. penambahan', 'd. perubahan','e. perubahan kecepatan'],
+        'correctAnswer': 2
+	}, {
+		'question': '9. Sebuah benda dilempar ke atas, akhirnya jatuh kebawah. Peristiwa itu terjadi karena adanya...',
+        'answers': ['a. gaya tarik', 'b. gaya otot', 'c. gaya jatuh', 'd. gaya gesek','e. gaya gravitasi bumi'],
+        'correctAnswer': 5
+	}, {
+		'question': '10. Satuan gaya menurut SI adalah...',
+        'answers': ['a. newton', 'b. joule', 'c. hertz', 'd. coulomb', 'e. ohm'],
+        'correctAnswer': 1
+	}, 
+	],
+    'resultComments': {
+        perfect: 'Perfecto Gan!, Pertahankan Prestasimu',
+        excellent: 'Bagus Banget gan!, Tigkatkan Lagi',
+        good: 'Bagus gan!, Lebih Giat Lagi Belajarnya!',
+        average: 'Mantap Gan!, Lebih Giat Lagi Belajarnya!',
+        bad: 'Kurang Gan!, Lebih Giat Lagi Belajarnya!',
+        poor: 'Kurang Gan!, Tingkatkan Lagi Belajar Ente',
+        worst: 'Sangat Kurang Gan!, Tingkatkan Lagi Waktu Belajar Ente'
+    }
+
+};
